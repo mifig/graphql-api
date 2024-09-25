13d9803e2737afc98587082cf18891827c4b048d
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2024-09-25 16:24:15 +0100

    trying to correct error page not showing in prod

--- frontend-ts/app/blogs/error.tsx
+++ frontend-ts/app/blogs/error.tsx
@@ -5,16 +5,16 @@ import Link from "next/link"
 export default function Error({ error }: { error: Error}) {
   try {
     const parsedError: {error: string, code: number} = JSON.parse(error.message)
+    return (
+      <div className="flex flex-col gap-5 items-center mt-20">
+        <h1 className="text-8xl font-bold">{parsedError.code}</h1>
+        <p>{parsedError.error}</p>
+  
+        {parsedError.code === 401 && <Link href="/login" className="bg-blue-700 px-3 py-2 text-yellow-200 hover:bg-blue-900">Login</Link>}
+      </div>
+    )
   } catch (e) {
     console.error("Error parsing error message:", e); // Log the parsing error for debugging
   }
 
-  return (
-    <div className="flex flex-col gap-5 items-center mt-20">
-      <h1 className="text-8xl font-bold">{parsedError.code}</h1>
-      <p>{parsedError.error}</p>
-
-      {parsedError.code === 401 && <Link href="/login" className="bg-blue-700 px-3 py-2 text-yellow-200 hover:bg-blue-900">Login</Link>}
-    </div>
-  )
 }

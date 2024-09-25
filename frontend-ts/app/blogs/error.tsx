d641110251193e34ba58a36a019b27261199ece3
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2024-09-25 16:40:01 +0100

    trying to correct error page not showing in prod

--- frontend-ts/app/blogs/error.tsx
+++ frontend-ts/app/blogs/error.tsx
@@ -14,7 +14,13 @@ export default function Error({ error }: { error: Error}) {
       </div>
     )
   } catch (e) {
-    console.error("Error parsing error message:", e); // Log the parsing error for debugging
+    return (
+      <div className="flex flex-col gap-5 items-center mt-20">
+        <h1 className="text-8xl font-bold">ERROR</h1>
+        <p>An Error occured</p>
+  
+        <Link href="/login" className="bg-blue-700 px-3 py-2 text-yellow-200 hover:bg-blue-900">Login</Link>
+      </div>
+    )
   }
-
 }
